9d26141bb30b62d619d0d4599720729875157556
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2020-02-05 21:26:19 +0100

    Add example

--- 01_min_glfw/src/main.cpp
+++ 01_min_glfw/src/main.cpp
@@ -11,8 +11,11 @@ int main(void) {
 	}
 
 	glfwMakeContextCurrent(window);
+	
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window) &&
+	    glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
